aef4adc777d9aee983a7ba4e03a5d328d233ee31
Alex Rueda U <alexruedau@gmail.com>
2019-01-22 23:40:12 +0100

    Making the Bill class

--- atm.js
+++ atm.js
@@ -1,1 +1,16 @@
-alert("Money!");
+class Bill {
+  constructor(v, q) { //value, quantity
+    this.myValue = v;
+    this.myQuantity = q;
+  }
+}
+
+var myBox = [];
+var delivered = [];
+myBox.push(new Bill(50, 3));
+myBox.push(new Bill(20, 2));
+myBox.push(new Bill(10, 2));
+
+var money = 210;
+var div = 0;
+var papers = 0;
